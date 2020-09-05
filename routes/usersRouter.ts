ae9187f120adf5f1a70cc82331b3bae8eab17be3
ClydeFrog04 <egan.randal@gmail.com>
2020-09-04 18:20:35 -0600

    feat(usersrouter.ts): login route
    Login capability created

--- routes/usersRouter.ts
+++ routes/usersRouter.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import * as usersModel from "../models/usersModel";
-import brcytjs from "bcryptjs";
+import brcypt from "bcryptjs";
+import {restrict} from "../middleware/restrict";
+import jwt from "jsonwebtoken";
 
 export const usersRouter = express.Router();
 
@@ -12,10 +14,9 @@ usersRouter.post("/register",  async (req, res) => {
 
         if(user) return res.status(409).json({message: "Username is already taken"});
 
-
         const newUser = await usersModel.add({
             username,
-            password: await brcytjs.hash(password, 13),
+            password: await brcypt.hash(password, 13),
             department
         });
         res.status(201).json(newUser);
@@ -23,14 +24,58 @@ usersRouter.post("/register",  async (req, res) => {
         console.log(e.stack);
         res.status(500).json({error: "Error registering new user"});
     }
+});
 
+usersRouter.post("/login", async (req, res) => {
+    try {
+        const {username, password, department} = req.body;
+        const user = await usersModel.findBy({username}).first();
+
+        if(!user) return res.status(401).json({message: "Username invalid"});
+
+        //hash the password and check that is matches what was found in db
+        const passValid = await brcypt.compare(password, user.password);
+        if(!passValid) return res.status(401).json({message: "Password invalid"});
+
+        //generate a new json web token
+        const token = jwt.sign({
+            userID: user.id,
+            department: user.department
+            //@ts-ignore: todo: same jwt error as before string | und
+        }, process.env.JWT_SECRET);
+
+        res.cookie("token", token);
+        res.status(200).json({token: token, userId: user.id});//again, sending the token in the response body because that's what the react students will expect in build week
+
+    } catch (e) {
+        console.log(e.stack);
+        res.status(500).json({error: "Error logging in"});
+    }
+});
+
+usersRouter.get("/users", restrict, async (req, res) => {
+    try {
+        res.json(await usersModel.find());//no need to do any more logic here because restrict contains our authorization login
+    } catch (e) {
+        console.log(e.stack);
+        res.status(500).json({error: "Error getting users"});
+    }
+
+    /*
+    router.get("/users", restrict("admin"), async (req, res, next) => {
+	try {
+		res.json(await Users.find())
+	} catch(err) {
+		next(err)
+	}
+})
+     */
 });
 
 /*
-POST	/api/register	Creates a user using the information sent inside the body of the request. Hash the password before saving the user to the database.
 
-POST	/api/login	Use the credentials sent inside the body to authenticate the user. On successful login, create a new JWT with the user id as the subject and send it back to the client. If login fails, respond with the correct status code and the message: 'You shall not pass!'
 
-GET	/api/users	If the user is logged in, respond with an array of all the users contained in the database. If the user is not logged in respond with the correct status code and the message: 'You shall not pass!'.
+GET	/api/users	If the user is logged in, respond with an array of all the users contained in the database. If the user is not logged in respond with the
+correct status code and the message: 'You shall not pass!'.
 
  */
